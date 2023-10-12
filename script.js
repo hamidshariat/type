5e1a74742828645819ad7aaa5f3c03b77041203b
hamidshariat <hamid13841147@gmail.com>
2023-10-12 16:40:37 +0330

    Code correction

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ hero.addEventListener('mousemove' , function(event) {
     y += event.target.offsetTop;
     const xWalk = Math.round((x / width * walk) - (walk / 2))
     const yWalk =  Math.round((y / height * walk) - (walk / 2))
-    console.log(xWalk , yWalk);
     text.style.textShadow = `
         ${xWalk}px ${yWalk}px 0 rgba(238 ,82,83,.7),
         ${xWalk * -1}px ${yWalk}px 0 rgba(52 ,31,151,.7),
